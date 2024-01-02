9a0f6675edf7c9ee9124933b6d8f66d523671266
xorelorax <allanschwenck@gmail.com>
2024-01-02 19:21:12 -0300

    Cronometro Simples

---
+++ Program.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Threading;
+namespace Cronometro
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Escolha uma opção.");
+            Console.WriteLine("M - Minutos => Ex: 1m");
+            Console.WriteLine("S - Segundos => Ex: 50s");
+            Console.WriteLine("0 = Sair");
+                        
+            // Para converter apenas o último caractere para lowercase. 
+             string data = Console.ReadLine().ToLower();
+             char type = char.Parse(data.Substring(data.Length - 1, 1));
+            // Para pegar apenas o último caractere já convertido.
+             int time = int.Parse(data.Substring(0, data.Length - 1));           
+             int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+            if (time == '0')
+                System.Environment.Exit(0);
+                
+                
+            Start(time * multiplier);
+        }
+        
+        static void Start(int time)
+        {            
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            
+            Console.Clear();
+            Console.WriteLine("Cronômetro finalizado");
+
+        }
+    }
+}
